1fb06d07d85f8faf6eb23619540e3751fac7fa9a
simonsbt <sabatiersimon1@gmail.com>
2022-12-03 11:25:20 +0100

    Initial Commit

---
+++ Le Baluchon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Le Baluchon
+//
+//  Created by Simon Sabatier on 03/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
